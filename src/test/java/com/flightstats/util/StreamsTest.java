38ebb949aca66ccfe8e306af7af23eab244a9795
Jason Plumb <jplumb@flightstats.com>
2014-09-29 13:01:14 -0700

    run the test a few times with success short-circuiting (to discourage random occasional failures)

--- src/test/java/com/flightstats/util/StreamsTest.java
+++ src/test/java/com/flightstats/util/StreamsTest.java
@@ -6,20 +6,17 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
-import static junit.framework.Assert.assertFalse;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
 
 public class StreamsTest {
 
     @Test
     public void testTimes() throws Exception {
         AtomicInteger resultCount = new AtomicInteger(0);
-    	Streams.times(100).forEach(x -> {
+        Streams.times(100).forEach(x -> {
             assertNull(x);
             resultCount.incrementAndGet();
         });
@@ -28,29 +25,30 @@ public class StreamsTest {
 
     @Test
     public void testTimes_parallelism() throws Exception {
-        Random random = new Random();
-        AtomicInteger resultCount = new AtomicInteger(0);
-        List<Integer> counter = Collections.synchronizedList(new ArrayList<>());
-        Streams.times(100).parallel().forEach(x -> {
-            try {
-                Thread.sleep(random.nextInt(10));
-                assertNull(x);
-                int i = resultCount.getAndIncrement();
-                counter.add(i);
-            } catch (InterruptedException e) {
-                //ignore this..test will fail in this case anyway.
-            }
-        });
-        assertEquals(100, resultCount.get());
-        int current = 0;
-        AtomicBoolean alwaysInOrder = new AtomicBoolean(true);
-        for (Integer integer : counter) {
-            if (integer != current++) {
-                alwaysInOrder.set(false);
+        boolean seenOutOfOrder = false;
+        for (int j = 0; !seenOutOfOrder && (j < 100); j++) {
+            Random random = new Random();
+            AtomicInteger resultCount = new AtomicInteger(0);
+            List<Integer> counter = Collections.synchronizedList(new ArrayList<>());
+            Streams.times(100).parallel().forEach(x -> {
+                try {
+                    Thread.sleep(random.nextInt(10));
+                    assertNull(x);
+                    int i = resultCount.getAndIncrement();
+                    counter.add(i);
+                } catch (InterruptedException e) {
+                    //ignore this..test will fail in this case anyway.
+                }
+            });
+            assertEquals(100, resultCount.get());
+            for (int i = 0; i < counter.size(); i++) {
+                 if(counter.get(i) != i){
+                     seenOutOfOrder = true;
+                 }
+
             }
         }
-
-        assertFalse(alwaysInOrder.get());
+        assertTrue(seenOutOfOrder);
     }
 
     @Test
