8691929b356c066c0da17cbbdb0b84efc5f9100c
Jason Plumb <jplumb@flightstats.com>
2014-10-06 16:04:51 -0700

    add javadocs

--- src/main/java/com/flightstats/util/BinaryOperators.java
+++ src/main/java/com/flightstats/util/BinaryOperators.java
@@ -4,6 +4,14 @@ import java.util.function.BinaryOperator;
 
 public class BinaryOperators {
 
+    /**
+     * Sometimes you are working with code that just picks the first item out of a stream or
+     * just arbitrarily picks one...and sometimes you wish to augment that code with a selection
+     * strategy of some kind (reduce()).  This BinaryOperator can help you to do that in a way that
+     * doesn't break backwards compatibility.
+     *
+     * @return A new BinaryOperator<T> that always just picks the first (leftmost) operand.
+     */
     public static <T> BinaryOperator<T> firstOne(){
         return (t, t2) -> t;
     }
